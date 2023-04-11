5b4f03064f7016095275760ea3bfecb9476cbc29
Per Berge <per.berge93@gmail.com>
2023-04-11 14:25:19 +0200

    (feat): Adding players

---
+++ src/components/AddPlayer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const player = ref('');
+const playerSymbol = ref(['X', 'O']);
+
+const addPlayer = () => {
+  emits('addPlayer', player);
+  player.value = '';
+};
+
+const emits = defineEmits(['addPlayer']);
+</script>
+
+<template>
+  <form @submit.prevent="addPlayer">
+    <label>Spelare {{ playerSymbol }}: </label>
+    <input placeholder="Playername" v-model="player" type="text" />
+    <button type="submit">Add Player</button>
+  </form>
+</template>
+
+<style scoped></style>
