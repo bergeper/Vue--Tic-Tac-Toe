821a8287667bfa2d37f38fe6adbe040e6867ccc1
Per Berge <per.berge93@gmail.com>
2023-04-11 18:37:11 +0200

    (feat): Localstorage in own files

---
+++ src/helpers/getPlayersFromLS.ts
@@ -0,0 +1,11 @@
+import { Player } from '../models/Player';
+
+let playersFromLS: Player[] = [];
+
+export function getFromLocalStorage(): Player[] {
+  playersFromLS = JSON.parse(localStorage.getItem('players') || '[]');
+  let players = playersFromLS.map((player) => {
+    return new Player(player.id, player.name, player.score);
+  });
+  return players;
+}
