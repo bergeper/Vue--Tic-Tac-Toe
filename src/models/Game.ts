9bd1c845e4a9279ee0127a56ebe8ee48791c1f1a
Per Berge <per.berge93@gmail.com>
2023-04-11 14:25:52 +0200

    (Feat): nothing to see here

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -2,8 +2,8 @@ import { Player } from './Player';
 
 export class Game {
   constructor(
-    public playerID: Player,
-    public spot: number,
+    public playerID: [Player],
+    public spot: [number],
     public taken: boolean
   ) {}
 }
