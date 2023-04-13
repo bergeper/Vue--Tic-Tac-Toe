3b33bb9ca0bfeb8770c07059433a6148497f6129
Per Berge <per.berge93@gmail.com>
2023-04-13 17:19:22 +0200

    (feat): now using symbol instead of id to track players

--- src/components/StartGame.vue
+++ src/components/StartGame.vue
@@ -7,64 +7,74 @@ import GameBoard from './GameBoard.vue';
 import { getFromLocalStorage } from '../helpers/getPlayersFromLS';
 import { savePlayerInLS } from '../helpers/saveToLS';
 import { Game } from '../models/Game';
+import { randomizePlayer } from '../helpers/randomizeStart';
 
 const playersFromLS = getFromLocalStorage();
+const showContent = ref({ showGame: false, showInput: true });
 
 const game = ref<Game>({
-  players: playersFromLS,
-  board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  players: [],
+  board: ['', '', '', '', '', '', '', '', ''],
+  activePlayer: new Player('', '', 0),
   isGameDone: false,
 });
 
-const changeTurn = ref(true);
-const showContent = ref({ showGame: false, showInput: true });
-
-const addPlayer = (playerName: string) => {
-  let id = 1;
-  if (game.value.players.length <= 1) {
-    if (game.value.players.length === 0) {
-      game.value.players.push(new Player(id, playerName, 0));
-      savePlayerInLS(game.value.players);
-    } else {
-      id++;
-      game.value.players.push(new Player(id, playerName, 0));
-      savePlayerInLS(game.value.players);
-      showContent.value.showGame = true;
-      showContent.value.showInput = false;
-    }
-  }
-};
-
 if (playersFromLS.length === 2) {
+  game.value.players.push(...playersFromLS);
+  const playerToStart = randomizePlayer(
+    game.value.players[0],
+    game.value.players[1]
+  );
+  game.value.activePlayer = playerToStart;
+  console.log(playerToStart);
+
   showContent.value.showGame = true;
   showContent.value.showInput = false;
 }
 
-const toggleSquare = (i: number) => {
-  if (changeTurn.value === true) {
-    game.value.board[i] = 1;
-    changeTurn.value = false;
-  } else {
-    game.value.board[i] = 2;
-    changeTurn.value = true;
+const addPlayer = (playerName: string) => {
+  if (game.value.players.length <= 1) {
+    if (game.value.players.length === 0) {
+      game.value.players.push(new Player('X', playerName, 0));
+      savePlayerInLS(game.value.players);
+    } else {
+      game.value.players.push(new Player('O', playerName, 0));
+      savePlayerInLS(game.value.players);
+      showContent.value.showGame = true;
+      showContent.value.showInput = false;
+      const playerToStart = randomizePlayer(
+        game.value.players[0],
+        game.value.players[1]
+      );
+      game.value.activePlayer = playerToStart;
+    }
   }
-  console.log(game.value.board);
 };
 
-const winnerwinnerchickendinner = () => {};
+const markSquare = (i: number) => {
+  if (game.value.activePlayer == game.value.players[0]) {
+    game.value.board[i] = game.value.activePlayer.symbol;
+    game.value.activePlayer = game.value.players[1];
+  } else {
+    game.value.board[i] = game.value.activePlayer.symbol;
+    game.value.activePlayer = game.value.players[0];
+  }
+};
 </script>
 
 <template>
   <AddPlayer @add-player="addPlayer" v-if="showContent.showInput"></AddPlayer>
-  <div class="gameboard">
-    <GameBoard
-      :player="game.board[index]"
-      :board="board"
-      v-for="(board, index) in game.board"
-      :key="index"
-      v-if="showContent.showGame"
-      @toggle-square="toggleSquare(index)"
-    ></GameBoard>
+  <div v-if="showContent.showGame">
+    <h3>Player-{{ game.activePlayer.symbol }}: {{ game.activePlayer.name }}</h3>
+    <div class="gameboard">
+      <GameBoard
+        :index="index"
+        :game="game"
+        v-for="(board, index) in game.board"
+        :key="index"
+        @toggle-square="markSquare(index)"
+      ></GameBoard>
+    </div>
   </div>
   <ShowScore></ShowScore>
 </template>
