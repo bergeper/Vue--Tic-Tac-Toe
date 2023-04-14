9441011331dfed5cdcf4b19964471e765696e734
Per Berge <per.berge93@gmail.com>
2023-04-14 18:37:50 +0200

    (feat): Shows winner

---
+++ src/components/ShowWinner.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Player } from '../models/Player';
+
+interface IWinner {
+  winner: Player;
+}
+
+const winner = defineProps<IWinner>();
+</script>
+
+<template>
+  <h2>The winner is: {{ winner.winner.name }}!</h2>
+  <button>New Game</button>
+</template>
+
+<style scoped lang="scss"></style>
