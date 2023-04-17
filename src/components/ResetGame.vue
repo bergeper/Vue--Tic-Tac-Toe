08b79af9529eae18e586e48fc9e70d9ecb79827a
Per Berge <per.berge93@gmail.com>
2023-04-17 13:05:38 +0200

    (feat): reseting state and clearing localstorage

--- src/components/ResetGame.vue
+++ src/components/ResetGame.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const emits = defineEmits(['resetGame']);
+</script>
 
 <template>
-  <button>Reset Game</button>
+  <button @click="emits('resetGame')">Reset Game</button>
 </template>
 
 <style scoped lang="scss"></style>
